ace399f011a815705a36d2789902188f97220877
Sarang837 <63908634+Sarang837@users.noreply.github.com>
2022-11-07 21:27:35 +0530

    added API for GET, PUT and DELETE posts

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    #local
+    #3rd party apps
+    "rest_framework",
+
+    #local apps
     "accounts.apps.AccountsConfig",
     "posts.apps.PostsConfig"
 ]
