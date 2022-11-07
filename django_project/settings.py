0f1ce2de73cdd3cdc482c78a482f68b431ea165c
Sarang837 <63908634+Sarang837@users.noreply.github.com>
2022-11-07 21:13:37 +0530

    added posts model

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     #local
-    "accounts.apps.AccountsConfig"
+    "accounts.apps.AccountsConfig",
+    "posts.apps.PostsConfig"
 ]
 
 MIDDLEWARE = [
